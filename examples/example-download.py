6503e9f0999700ae5075bf4934cb7f057522b685
Tim Clements <thclements@g.harvard.edu>
2019-07-03 12:50:43 -0400

    update example

--- examples/example-download.py
+++ examples/example-download.py
@@ -1,6 +1,6 @@
 import sys
 import os
-sys.path.append(os.path.abspath("~/SCEDC-AWS/src/download_data/"))
+sys.path.append(os.path.abspath("/home/ubuntu/SCEDC-AWS/src/download_data/"))
 from scedc_download import download
 import datetime
 starttime = datetime.datetime(2016,7,4)
@@ -12,4 +12,4 @@ channel = "HH*"
 # maxlatitude = 34.
 # minlatitude = 32.
 OUTDIR = "~/data/"
-%timeit -n1 -r1 download(OUTDIR,starttime=starttime,endtime=endtime,network=network,channel=channel)
+download(OUTDIR,starttime=starttime,endtime=endtime,network=network,channel=channel)
