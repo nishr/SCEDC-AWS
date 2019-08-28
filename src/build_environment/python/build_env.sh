533e8cc6ce6ff3b017d8d94f3688b0a51e4eb05e
Tim Clements <thclements@g.harvard.edu>
2019-08-28 16:40:30 -0400

    Update python install

--- src/build_environment/python/build_env.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-conda env create -f ./py3.yml
-conda clean -tipsy # clean up cache. Saves ~600 MB space
-conda init bash
-source ~/.bashrc
--- src/build_environment/python/install_miniconda.sh
+++ src/build_environment/python/install_miniconda.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
-bash miniconda.sh -b -p $HOME/miniconda
+if [ -d "/shared" ]
+then
+    bash miniconda.sh -b -p /shared/miniconda
+echo 'export PATH="/shared/miniconda/bin:$PATH"' >> ~/.bashrc
+else
+    bash miniconda.sh -b -p $HOME/miniconda
+    echo 'export PATH="$HOME/miniconda/bin:$PATH"' >> ~/.bashrc
+fi
 rm miniconda.sh
-echo 'export PATH="$HOME/miniconda/bin:$PATH"' >> ~/.bashrc
 source ~/.bashrc
+
+# create environment
+conda env create -f ~/SCEDC-AWS/src/build_environment/python/py3.yml
+conda clean -tipsy # clean up cache. Saves ~600 MB space
+conda init bash
+source ~/.bashrc
+conda activate py3
+
