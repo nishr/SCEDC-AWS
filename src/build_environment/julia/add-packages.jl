dffc11f2e981073b99329e984b10638914048906
Tim Clements <thclements@g.harvard.edu>
2020-03-02 11:03:27 -0500

    add Glob and Benchmarktools

--- src/build_environment/julia/add-packages.jl
+++ src/build_environment/julia/add-packages.jl
@@ -2,6 +2,6 @@ using Pkg
 
 # add following packages
 Pkg.add(["AWSCore","AWSS3","DataFrames", "DSP", "FFTW", "Glob", "JLD2", 
-       "Interpolations", "GLM", "Plots","CSV"])
+       "Interpolations", "GLM", "Plots","CSV", "Glob","BenchmarkTools"])
 Pkg.add(PackageSpec(name="SeisIO", rev="master"))
 Pkg.add(PackageSpec(name="SeisNoise", rev="master"))
