385c5f97d8eb9b394386a2d4b1d25470b9dd6c44
Tim Clements <thclements@g.harvard.edu>
2020-04-14 23:12:38 -0400

    Update to Julia 1.4

--- src/build_environment/julia/add-packages.jl
+++ src/build_environment/julia/add-packages.jl
@@ -4,4 +4,4 @@ using Pkg
 Pkg.add.(["AWSCore","AWSS3","DataFrames", "DSP", "FFTW", "Glob", "JLD2", 
        "Interpolations", "GLM", "Plots","CSV", "Glob","BenchmarkTools"])
 Pkg.add(PackageSpec(name="SeisIO", rev="master"))
-Pkg.add(PackageSpec(name="SeisNoise", rev="master"))
+Pkg.add(PackageSpec(name="SeisNoise", rev="GPU"))
